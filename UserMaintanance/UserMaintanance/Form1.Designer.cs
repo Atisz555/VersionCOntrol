d3bdee13b1e0e4e3216836042c5d3e9c03dd5cb6
Attila Péter Tábi <attila.tabi@stud.uni-corvinus.hu>
2023-03-07 09:22:34 +0100

    kalozkodunk

--- UserMaintanance/UserMaintanance/Form1.Designer.cs
+++ UserMaintanance/UserMaintanance/Form1.Designer.cs
@@ -31,9 +31,7 @@ namespace UserMaintanance
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -47,39 +45,23 @@ namespace UserMaintanance
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(388, 64);
+            this.textBox1.Location = new System.Drawing.Point(388, 68);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(150, 20);
             this.textBox1.TabIndex = 1;
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(388, 98);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(150, 20);
-            this.textBox2.TabIndex = 2;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(283, 64);
+            this.label1.Location = new System.Drawing.Point(283, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(283, 98);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(286, 158);
+            this.button1.Location = new System.Drawing.Point(286, 122);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(252, 23);
             this.button1.TabIndex = 5;
@@ -93,9 +75,7 @@ namespace UserMaintanance
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -110,9 +90,7 @@ namespace UserMaintanance
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
     }
 }
--- UserMaintanance/UserMaintanance/Form1.cs
+++ UserMaintanance/UserMaintanance/Form1.cs
@@ -17,8 +17,7 @@ namespace UserMaintanance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
         }
 
@@ -35,8 +34,7 @@ namespace UserMaintanance
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
             };
             users.Add(u);
         }
