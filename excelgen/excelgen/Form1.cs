f599b74a7ce96e760ea65e988e70c74ee0030930
Attila Péter Tábi <attila.tabi@stud.uni-corvinus.hu>
2023-03-14 08:53:47 +0100

    kozben

--- excelgen/excelgen/Form1.cs
+++ excelgen/excelgen/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace excelgen
 {
     public partial class Form1 : Form
     {
+        List<Flat> flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void LoadData()
+        {
+            flats = context.Flats.ToList();        
         }
     }
 }
