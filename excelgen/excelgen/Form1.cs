07fdf1cca34d61e22d9d40b48d8e35482a181060
Attila Péter Tábi <attila.tabi@stud.uni-corvinus.hu>
2023-03-14 09:02:42 +0100

    aa

--- excelgen/excelgen/Form1.cs
+++ excelgen/excelgen/Form1.cs
@@ -16,20 +16,49 @@ namespace excelgen
     {
         List<Flat> flats;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
+
         void LoadData()
         {
             flats = context.Flats.ToList();        
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errorMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errorMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
